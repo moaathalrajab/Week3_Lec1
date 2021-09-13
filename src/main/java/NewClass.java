ea484723e94df326780a2ffa052df28010f6cc59
Moaath Alrajab <>
2021-09-13 09:48:59 -0400

    Initial Commit - project created

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,16 @@
+
+public class NewClass {
+    
+    public static void main(String[] args) {
+        
+       int x=0, y=10;
+        
+        do{
+            
+            System.out.println("i = " +x );
+            
+            
+        }while(x!=0) ;
+    }
+    
+}
