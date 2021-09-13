26d205e99be1170a97577009124820b417953b22
Moaath Alrajab <>
2021-09-13 10:25:27 -0400

    Add array object wit init and access

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,16 +1,13 @@
-
-public class NewClass {
-    
-    public static void main(String[] args) {
-        
-       int x=0, y=10;
-        
-        do{
-            
-            System.out.println("i = " +x );
-            
-            
-        }while(x!=0) ;
-    }
-    
+public class NewClass {  
+    public static void main(String[] args) {   
+       int[] arr = new int[10];
+       // initialize the elements of the array
+       for(int i=0; i< arr.length; i++){
+            arr [i] = 10 + i;
+       }
+       // access their values
+       for(int i=0; i< arr.length; i++){
+           System.out.println("Arr["+i+"] : "+ arr [i]);
+       }
+    } 
 }
