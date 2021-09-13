6ed9b8600bfed2502bd67478731bf57a289a57bf
Moaath Alrajab <soblab@WHIT208W10_00.it.farmingdale.edu>
2021-09-13 10:38:07 -0400

    2 dim array init and access

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,15 +1,16 @@
 public class NewClass {  
     public static void main(String[] args) {   
-       int[] arr = new int[10];
+       int arr[][] = new int[3][5];
        // initialize the elements of the array
-       for(int i=0; i< arr.length; i++){
-            arr [i] = 10 + i;
-       }
+       for(int i=0; i< 3; i++){
+           for(int j=0; j< 5; j++){
+            arr [i][j] = 10 + i;
+       }}
        // access their values 
-       
-       
-       for(int i=0; i< arr.length; i++){
-           System.out.println("Arr["+i+"] : "+ arr [i]);
+       for(int i=0; i< 3; i++){
+           for(int j=0; j< 5; j++){
+           System.out.println("Arr["+i+", "+j+"] : "+ arr [i][j]);
+       }
        }
     } 
 }
